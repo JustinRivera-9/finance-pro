883a3e6cb287490b4df91ef68d199c0ab9352fa7
JustinRivera-9 <justin_rivera9@outlook.com>
2023-11-17 13:36:33 -0700

    added logout functionality

--- src/pages/app/account/Account.jsx
+++ src/pages/app/account/Account.jsx
@@ -1,12 +1,23 @@
 import { Button } from "@mui/material";
+import { useNavigate } from "react-router";
+import supabase from "../../../config/supabaseClient";
 
 function Account() {
+  const navigate = useNavigate();
+
+  async function signOutUser() {
+    const { error } = await supabase.auth.signOut();
+    navigate("/");
+  }
+
   return (
     <div>
       <h1 className="flex justify-center text-3xl mt-24">
         Update settings, logout, change password, etc.
       </h1>
-      <Button size="large">LOGOUT</Button>
+      <Button onClick={() => signOutUser()} size="large">
+        LOGOUT
+      </Button>
     </div>
   );
 }
