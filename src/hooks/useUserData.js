e319029b06c356dfcdc1d7c0bdf3668b8688ad3c
JustinRivera-9 <justin_rivera9@outlook.com>
2023-11-17 13:35:31 -0700

    created useUserData hook

---
+++ src/hooks/useUserData.js
@@ -0,0 +1,23 @@
+import supabase from "../config/supabaseClient";
+import { useState, useEffect } from "react";
+import { useNavigate } from "react-router";
+
+export default function useUserData() {
+  const [user, setUser] = useState({});
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    async function fetchUserData() {
+      setIsLoading(true);
+      await supabase.auth.getUser().then((value) => {
+        if (value.data?.user) {
+          setUser(value.data.user);
+        }
+      });
+      setIsLoading(false);
+    }
+    fetchUserData();
+  }, []);
+
+  return { user, isLoading };
+}
