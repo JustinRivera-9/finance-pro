fe8de704f40f5d08f3deda8e6ce81cc825c2b2f6
JustinRivera-9 <justin_rivera9@outlook.com>
2023-11-20 21:47:32 -0700

    moved auth change logic to AppLayout

--- src/components/AuthForm.jsx
+++ src/components/AuthForm.jsx
@@ -1,20 +1,8 @@
 import { Auth } from "@supabase/auth-ui-react";
-import { useNavigate } from "react-router";
 import supabase from "../config/supabaseClient";
 import { ThemeSupa } from "@supabase/auth-ui-shared";
 
 function AuthForm() {
-  const navigate = useNavigate();
-
-  supabase.auth.onAuthStateChange(async (event) => {
-    console.log(event);
-    if (event !== "SIGNED_OUT") {
-      // navigate("/app");
-    } else {
-      navigate("/");
-    }
-  });
-
   return (
     <div className="flex justif-center">
       <div className="w-1/3 rounded-xl bg-zinc-700 px-8 py-4 mx-auto">
