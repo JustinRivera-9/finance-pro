da44f9fa0a306df1b789abf2e653722f7d20f040
JustinRivera-9 <justin_rivera9@outlook.com>
2023-11-22 01:01:01 -0700

    added backdrop onClick to close form

--- src/components/app/budget/UpdateForm.jsx
+++ src/components/app/budget/UpdateForm.jsx
@@ -35,7 +35,7 @@ function UpdateForm({ formOpen, setFormOpen, onSubmit }) {
   }
 
   return (
-    <Backdrop open={formOpen}>
+    <Backdrop open={formOpen} onClick={handleCancel}>
       <div className="flex flex-col bg-white p-8 rounded-xl">
         <form className="flex flex-col" onSubmit={handleForm}>
           <TextField
