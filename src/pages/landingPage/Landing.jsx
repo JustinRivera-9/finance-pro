d98bf6110267bf8d4ec72682892dc19b4db8f818
JustinRivera-9 <justin_rivera9@outlook.com>
2023-11-22 00:27:24 -0700

    need to wrap navigate in effect

--- src/pages/landingPage/Landing.jsx
+++ src/pages/landingPage/Landing.jsx
@@ -4,6 +4,7 @@ import { Link, useNavigate } from "react-router-dom";
 
 function LandingPage({ userRole }) {
   const navigate = useNavigate();
+
   if (userRole === "authenticated") navigate("/app");
 
   return (
