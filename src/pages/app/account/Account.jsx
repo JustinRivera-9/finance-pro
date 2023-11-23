fe9f2f2817716c5d36c4e2f36dbe4b2123058a45
JustinRivera-9 <justin_rivera9@outlook.com>
2023-11-23 01:39:27 -0700

    now reading from database

--- src/pages/app/account/Account.jsx
+++ src/pages/app/account/Account.jsx
@@ -1,8 +1,13 @@
 import { Button } from "@mui/material";
 import { useNavigate } from "react-router";
 import supabase from "../../../config/supabaseClient";
+import useGetSettings from "../../../hooks/useGetSettings";
 
 function Account() {
+  const { settings, isLoading, error } = useGetSettings(
+    "090886e5-3913-48bd-8582-6aa150cf0cf2"
+  );
+  const { first_name, last_name, email } = settings;
   const navigate = useNavigate();
 
   async function signOutUser() {
@@ -11,9 +16,18 @@ function Account() {
   }
 
   return (
-    <div>
-      <h1 className="flex justify-center text-3xl mt-24">
-        Update settings, logout, change password, etc.
+    <div className="flex flex-col justify-center text-3xl mt-24">
+      <h1>
+        <strong>First Name: </strong>
+        {first_name}
+      </h1>
+      <h1>
+        <strong>Last Name: </strong>
+        {last_name}
+      </h1>
+      <h1>
+        <strong>Email: </strong>
+        {email}
       </h1>
       <Button onClick={() => signOutUser()} size="large">
         Sign Out
