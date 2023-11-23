e8b34c706b020821ce215ba95ec7d2629adfd7f6
JustinRivera-9 <justin_rivera9@outlook.com>
2023-11-23 01:39:41 -0700

    removed test code

--- src/pages/app/budget/OverviewBudget.jsx
+++ src/pages/app/budget/OverviewBudget.jsx
@@ -1,12 +1,7 @@
-import useGetSettings from "../../../hooks/useGetSettings";
-
 function OverviewBudget() {
-  const { settings } = useGetSettings("090886e5-3913-48bd-8582-6aa150cf0cf2");
-
-  console.log(settings);
   return (
-    <div className="flex justify-center text-3xl mt-24">
-      <h1>INSERT BUDGET OVERVIEW</h1>
+    <div className="flex flex-col justify-center text-3xl mt-24">
+      <h1>INSERT OVERVIEW PAGE</h1>
     </div>
   );
 }
