709c0e33e810bde48f0375c78133b09a2b8aa5af
JustinRivera-9 <justin_rivera9@outlook.com>
2023-11-22 23:59:25 -0700

    syntax updates. fetches ID from users table

--- src/hooks/useUserData.js
+++ src/hooks/useUserData.js
@@ -1,51 +1,5 @@
-// import supabase from "../config/supabaseClient";
-// import { useState, useEffect } from "react";
-// import { useNavigate } from "react-router";
-
-// export default function useUserData() {
-//   const [userId, setUserId] = useState({});
-//   const [userdata, setUserData] = useState({});
-//   const [isLoading, setIsLoading] = useState(false);
-
-//   // console.log(user);
-
-//   useEffect(() => {
-//     async function fetchUserData() {
-//       try {
-//         setIsLoading(true);
-
-//         ////////// Get authenticated user's ID
-//         await supabase.auth.getUser().then((value) => {
-//           if (value.data?.user.id) {
-//             setUserId(value.data.user.id);
-//           }
-//         });
-
-//         ////////// Fetch user data based on ID
-//         let { data: users, error } = await supabase
-//           .from("users")
-//           .select("user_id")
-//           .eq("user_id", userId);
-
-//         console.log(users);
-//         // setUserData(users)
-
-//         setIsLoading(false);
-//       }
-//     } catch (err) {
-//       console.log(err)
-//     } finally {
-//       setIsLoading(false)
-//     }
-
-//     fetchUserData();
-//   }, [userId]);
-
-//   return { userId, isLoading };
-// }
 import supabase from "../config/supabaseClient";
 import { useState, useEffect } from "react";
-import { useNavigate } from "react-router";
 
 export default function useUserData() {
   const [userId, setUserId] = useState({});
@@ -66,12 +20,13 @@ export default function useUserData() {
 
         ////////// Fetch user data based on ID
         const { data: users, error } = await supabase
-          .from("user_activity")
+          .from("users")
           .select("*")
           .eq("user_id", userId);
-        setUserData(users);
 
         console.log(users);
+        setUserData(users);
+        if (error) throw new Error(error.message);
       } catch (err) {
         console.error(err);
       } finally {
