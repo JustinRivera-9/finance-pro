834b4ff739428ec7a4823b172c4ad923ebd4ae60
JustinRivera-9 <justin_rivera9@outlook.com>
2023-11-24 12:22:23 -0700

    Updated to use auth ID

--- src/hooks/useGetSettings.js
+++ src/hooks/useGetSettings.js
@@ -6,26 +6,27 @@ function useGetSettings(id) {
   const [isLoading, setIsLoading] = useState(true);
   const [error, setError] = useState(null);
 
-  const fetchUserSettings = async () => {
-    try {
-      const { data, error } = await supabase
-        .from("settings")
-        .select("*")
-        .eq("user_id", id);
-
-      if (error) throw new Error(error.message);
-      setSettings(data[0]);
-    } catch (err) {
-      console.error(err.message);
-      setError(err.message);
-    } finally {
-      setIsLoading(false);
-    }
-  };
-
   useEffect(() => {
+    const fetchUserSettings = async () => {
+      try {
+        const { data, error } = await supabase
+          .from("settings")
+          .select("*")
+          .eq("user_id", id);
+
+        if (error) throw new Error(error.message);
+        setSettings(data[0]);
+      } catch (err) {
+        console.error(err.message);
+        setError(err.message);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    // useEffect(() => {
     fetchUserSettings();
-  }, []);
+  }, [id]);
 
   return { settings, isLoading, error };
 }
