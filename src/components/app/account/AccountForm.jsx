8f5847d16372b012a1661c8bb937e6f1f06b2a22
JustinRivera-9 <justin_rivera9@outlook.com>
2023-11-24 13:40:03 -0700

    Created account form. Data passes up to account

---
+++ src/components/app/account/AccountForm.jsx
@@ -0,0 +1,59 @@
+import { Backdrop, Button, TextField } from "@mui/material";
+import { useState } from "react";
+
+function AccountForm({ formOpen, setFormOpen, onSubmit }) {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+
+  function handleForm(e) {
+    e.preventDefault();
+    onSubmit({ firstName, lastName });
+    setFormOpen(false);
+    resetForm();
+  }
+
+  function handleCancel() {
+    setFormOpen(false);
+    resetForm();
+  }
+
+  function resetForm() {
+    setFirstName("");
+    setLastName("");
+  }
+
+  return (
+    <Backdrop open={formOpen}>
+      <div className="flex flex-col bg-white p-8 rounded-xl">
+        <form className="flex flex-col" onSubmit={handleForm}>
+          <TextField
+            sx={{ marginTop: "1.5rem" }}
+            id="outlined-required"
+            label="First Name"
+            onChange={(e) => setFirstName(e.target.value)}
+            value={firstName}
+          />
+          <TextField
+            sx={{ marginTop: "1.5rem" }}
+            id="outlined-required"
+            label="Last Name"
+            onChange={(e) => setLastName(e.target.value)}
+            value={lastName}
+          />
+          <Button
+            variant="contained"
+            type="submit"
+            sx={{ marginTop: "1.5rem" }}
+          >
+            Save
+          </Button>
+          <Button sx={{ marginTop: ".75rem" }} onClick={handleCancel}>
+            Cancel
+          </Button>
+        </form>
+      </div>
+    </Backdrop>
+  );
+}
+
+export default AccountForm;
