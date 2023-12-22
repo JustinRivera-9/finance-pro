27bcc6c515da436526c7da1163b754bda131ef77
JustinRivera-9 <justin_rivera9@outlook.com>
2023-12-21 22:04:02 -0700

    added currency formatter function

--- src/config/helperFunctions.js
+++ src/config/helperFunctions.js
@@ -1,3 +1,12 @@
+export const capitalize = (str) => str.charAt(0).toUpperCase() + str.slice(1);
+
+export const formatCurrency = (value) => {
+  return new Intl.NumberFormat("en", {
+    style: "currency",
+    currency: "USD",
+  }).format(value);
+};
+
 export const shuffleArray = (arr) => {
   for (let i = arr.length - 1; i > 0; i--) {
     const j = Math.floor(Math.random() * (i + 1));
@@ -5,5 +14,3 @@ export const shuffleArray = (arr) => {
   }
   return arr;
 };
-
-export const capitalize = (str) => str.charAt(0).toUpperCase() + str.slice(1);
