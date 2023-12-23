d873d204eb0a22357d9b38f6ac8c6ebe184908c7
JustinRivera-9 <justin_rivera9@outlook.com>
2023-12-23 01:21:13 -0700

    removed unused components

--- src/pages/app/resources/OverviewResources.jsx
+++ src/pages/app/resources/OverviewResources.jsx
@@ -1,10 +1,8 @@
 import { Outlet } from "react-router";
-import NavBarResources from "../../../components/app/resources/NavBarResources";
 
 function OverviewResources() {
   return (
     <div>
-      <NavBarResources />
       <Outlet />
     </div>
   );
