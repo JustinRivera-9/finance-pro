f2c7ab489742b818991297bc1c00b3c49a98562a
JustinRivera-9 <justin_rivera9@outlook.com>
2023-12-23 22:00:01 -0700

    updated styles

--- src/components/app/budget/NavBarBudget.jsx
+++ src/components/app/budget/NavBarBudget.jsx
@@ -2,7 +2,7 @@ import { NavLink } from "react-router-dom";
 
 function NavBarBudget() {
   return (
-    <div className="flex justify-center space-x-8 text-lg text-slate-200">
+    <div className="flex justify-around mx-2 text-lg border-b pb-2 border-slate-200 text-slate-200">
       <NavLink to="/app/budget-tracking/">Overview</NavLink>
       <NavLink to="/app/budget-tracking/budget">Budget</NavLink>
       <NavLink to="/app/budget-tracking/update">Update</NavLink>
