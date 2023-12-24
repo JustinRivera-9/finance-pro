6451a6f53cdcefa2bf45b7158de4f5421f184e6d
JustinRivera-9 <justin_rivera9@outlook.com>
2023-12-24 11:51:02 -0700

    Completed

--- src/components/app/budget/BudgetCategoriesTotals.jsx
+++ src/components/app/budget/BudgetCategoriesTotals.jsx
@@ -1,6 +1,34 @@
+import { formatCurrency } from "../../../config/helperFunctions";
+
 function BudgetCategoriesTotals({ categories }) {
-  // need to update database total on each new expense
-  return <div>TOTALS</div>;
+  const totalIncome = categories
+    .filter((el) => el.type === "income")
+    .map((el) => el.amount)
+    .reduce((acc, cur) => acc + cur, 0);
+  const totalExpenses = categories
+    .filter((el) => el.type === "expense")
+    .map((el) => el.amount)
+    .reduce((acc, cur) => acc + cur, 0);
+  const net = totalIncome - totalExpenses;
+
+  return (
+    <div className="flex justify-between items-center w-10/12">
+      <div className="text-center">
+        <p className="text-green-300">Total Income</p>
+        <p>{formatCurrency(totalIncome)}</p>
+      </div>
+      <p className="text-2xl">-</p>
+      <div className="text-center">
+        <p className="text-red-300">Total Expenses</p>
+        <p>{formatCurrency(totalExpenses)}</p>
+      </div>
+      <p className="text-2xl">=</p>
+      <div className="text-center">
+        <p className="text-blue-500">Net Income</p>
+        <p>{formatCurrency(net)}</p>
+      </div>
+    </div>
+  );
 }
 
 export default BudgetCategoriesTotals;
