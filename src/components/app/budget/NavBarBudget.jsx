d1850e14b2250280b8bee7857a9dd75a7ee038e3
JustinRivera-9 <justin_rivera9@outlook.com>
2023-12-25 21:10:56 -0700

    added responsive styles

--- src/components/app/budget/NavBarBudget.jsx
+++ src/components/app/budget/NavBarBudget.jsx
@@ -2,7 +2,7 @@ import { NavLink } from "react-router-dom";
 
 function NavBarBudget() {
   return (
-    <div className="flex justify-around mx-2 text-lg border-b pb-2 border-slate-200 text-slate-200">
+    <div className="sm:flex sm:justify-center sm:border-none sm:space-x-8 flex justify-around mx-2 text-lg border-b pb-2 border-slate-200 text-slate-200">
       <NavLink to="/app/budget-tracking/">Overview</NavLink>
       <NavLink to="/app/budget-tracking/budget">Budget</NavLink>
       <NavLink to="/app/budget-tracking/update">Update</NavLink>
