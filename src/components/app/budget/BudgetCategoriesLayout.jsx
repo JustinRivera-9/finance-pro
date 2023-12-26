65397ba18b737bd998a194a438be8f534353f8ee
JustinRivera-9 <justin_rivera9@outlook.com>
2023-12-25 21:11:12 -0700

    added responsive design to columns

--- src/components/app/budget/BudgetCategoriesLayout.jsx
+++ src/components/app/budget/BudgetCategoriesLayout.jsx
@@ -2,7 +2,7 @@ import { Chip, Divider } from "@mui/material";
 import BudgetCategoriesCard from "./BudgetCategoriesCard";
 import SetUpMessage from "./SetUpMessage";
 
-const boxStyles = "flex flex-wrap justify-center gap-4 my-8 md:w-1/2";
+const boxStyles = "flex flex-wrap justify-center gap-4 my-8";
 
 function BudgetCategoriesLayout({ categories, setUp, openForm }) {
   const incomeArr = categories.filter((el) => el.type === "income");
@@ -13,23 +13,27 @@ function BudgetCategoriesLayout({ categories, setUp, openForm }) {
   }
 
   return (
-    <div className="flex flex-col justify-center md:flex-row">
-      <h2 className="mx-auto text-stone-700 text-xl bg-red-300 rounded-full py-2 px-10 font-semibold">
-        Expenses
-      </h2>
-      <ul className={boxStyles}>
-        {expenseArr.map((el) => (
-          <BudgetCategoriesCard key={el.id} budgetData={el} />
-        ))}
-      </ul>
-      <h2 className="mx-auto text-stone-700 text-xl font-semibold bg-green-400 rounded-full py-2 px-10">
-        Income
-      </h2>
-      <ul className={boxStyles}>
-        {incomeArr.map((el) => (
-          <BudgetCategoriesCard key={el.id} budgetData={el} />
-        ))}
-      </ul>
+    <div className="flex flex-col justify-center sm:flex-row sm:justify-around px-4">
+      <div className="">
+        <h2 className="w-fit mx-auto text-stone-700 text-xl bg-red-300 rounded-full py-2 px-10 font-semibold">
+          Expenses
+        </h2>
+        <ul className={boxStyles}>
+          {expenseArr.map((el) => (
+            <BudgetCategoriesCard key={el.id} budgetData={el} />
+          ))}
+        </ul>
+      </div>
+      <div>
+        <h2 className="w-fit mx-auto text-stone-700 text-xl font-semibold bg-green-400 rounded-full py-2 px-10">
+          Income
+        </h2>
+        <ul className={boxStyles}>
+          {incomeArr.map((el) => (
+            <BudgetCategoriesCard key={el.id} budgetData={el} />
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
