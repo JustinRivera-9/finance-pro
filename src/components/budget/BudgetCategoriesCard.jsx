a1e2544dd11bbaa8dd0e64614085bdef85807652
JustinRivera-9 <justin_rivera9@outlook.com>
2024-01-13 23:05:22 -0700

    UI - finalized design for setting budget

--- src/components/budget/BudgetCategoriesCard.jsx
+++ src/components/budget/BudgetCategoriesCard.jsx
@@ -1,17 +1,17 @@
-import RemoveCircleRoundedIcon from "@mui/icons-material/RemoveCircleRounded";
+import { FaPen } from "react-icons/fa";
+import { formatCurrency } from "../../utils/helperFunctions";
 
 function BudgetCategoriesCard({ budgetData }) {
   const { category, amount, type, id } = budgetData;
-  const budgetType = type === "Expense" ? "sky-700" : "sky-700";
 
   return (
     <li className="flex rounded-xl items-center justify-between p-4 text-xl font-normal bg-[#404040] text-stone-200">
-      <div className="mr-2">
+      <div className="flex justify-between space-x-12">
         <p className="capitalize">{category}</p>
-        <p className="">${amount}</p>
+        <p>{formatCurrency(amount)}</p>
       </div>
-      <button className="self-start" onClick={() => {}}>
-        <RemoveCircleRoundedIcon />
+      <button onClick={() => {}}>
+        <FaPen />
       </button>
     </li>
   );
--- src/components/budget/BudgetCategoriesLayout.jsx
+++ src/components/budget/BudgetCategoriesLayout.jsx
@@ -1,8 +1,8 @@
-import { Chip, Divider } from "@mui/material";
 import BudgetCategoriesCard from "./BudgetCategoriesCard";
 import SetUpMessage from "./SetUpMessage";
 
-const boxStyles = "flex flex-wrap justify-center gap-4 my-8";
+// const boxStyles = "flex flex-wrap justify-center gap-4 my-8";
+const boxStyles = "flex flex-col space-y-4 my-4";
 
 function BudgetCategoriesLayout({ categories, setUp, openForm }) {
   const incomeArr = categories.filter((el) => el.type === "income");
@@ -14,22 +14,22 @@ function BudgetCategoriesLayout({ categories, setUp, openForm }) {
 
   return (
     <div className="flex flex-col justify-center sm:flex-row sm:justify-around px-4">
-      <div className="">
-        <h2 className="w-fit mx-auto text-stone-700 text-xl bg-red-300 rounded-full py-2 px-10 font-semibold">
-          Expenses
-        </h2>
-        <ul className={boxStyles}>
-          {expenseArr.map((el) => (
-            <BudgetCategoriesCard key={el.id} budgetData={el} />
-          ))}
-        </ul>
-      </div>
-      <div>
-        <h2 className="w-fit mx-auto text-stone-700 text-xl font-semibold bg-green-400 rounded-full py-2 px-10">
+      <div className="md:w-1/3">
+        <h2 className="w-fit mx-auto text-stone-700 text-xl font-semibold bg-green-400 rounded-full py-2 px-14">
           Income
         </h2>
         <ul className={boxStyles}>
           {incomeArr.map((el) => (
+            <BudgetCategoriesCard key={el.id} budgetData={el} />
+          ))}
+        </ul>
+      </div>
+      <div className="md:w-1/3">
+        <h2 className="w-fit mx-auto text-stone-700 text-xl bg-red-300 rounded-full py-2 px-14 font-semibold">
+          Expenses
+        </h2>
+        <ul className={boxStyles}>
+          {expenseArr.map((el) => (
             <BudgetCategoriesCard key={el.id} budgetData={el} />
           ))}
         </ul>
