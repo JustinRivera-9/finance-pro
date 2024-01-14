0a199f97e17295f0e7ab4301d08d3cd93f37a0b0
JustinRivera-9 <justin_rivera9@outlook.com>
2024-01-14 13:01:28 -0700

    now fetching data using react query

--- src/pages/app/budget/Update.jsx
+++ src/pages/app/budget/Update.jsx
@@ -2,83 +2,94 @@ import SetUpMessage from "../../../components/budget/SetUpMessage";
 import UpdateForm from "../../../components/budget/UpdateForm";
 import { Button } from "@mui/material";
 import { useState } from "react";
-import useGetBudgetCategories from "../../../services/useGetBudgetCategories";
 import LoadingSpinner from "../../../ui/LoadingSpinner.jsx";
 import BudgetCategoriesLayout from "../../../components/budget/BudgetCategoriesLayout";
 import BudgetCategoriesTotals from "../../../components/budget/BudgetCategoriesTotals";
 import { v4 as uuidv4 } from "uuid";
+import { useQuery } from "@tanstack/react-query";
+import { getBudgetGoal } from "../../../services/budget/apiBudgetGoal.js";
 
 function Update({ userId }) {
-  // Need to memoize useGetTargetBudget hook call
-  const { data, isLoading, error } = useGetBudgetCategories(userId);
   const [formOpen, setFormOpen] = useState(false);
 
-  // form submit handler - NEED TO SET DATA
-  async function handleAddCategory(formData) {
-    const { category, amount, type } = formData;
+  // Budget goals query
+  const {
+    data: budgetGoals,
+    isLoading,
+    error,
+  } = useQuery({
+    queryKey: ["budgetGoals", userId],
+    queryFn: () => getBudgetGoal(userId),
+  });
 
-    // Check if data is available
-    if (data && data.categories) {
-      const updatedCategories = [
-        ...data.categories,
-        { id: uuidv4(), category, amount, type },
-      ];
-      const JSONcategories = JSON.stringify(updatedCategories);
+  console.log(budgetGoals, isLoading, error);
 
-      // const { data, error } = await supabase
-      //   .from("budget")
-      //   .update({ categories })
-      //   .eq("user_id", userId);
+  //   // form submit handler - NEED TO SET DATA
+  //   async function handleAddCategory(formData) {
+  //     const { category, amount, type } = formData;
 
-      // console.log(data, error);
-    }
-  }
+  //     // Check if data is available
+  //     if (data && data.categories) {
+  //       const updatedCategories = [
+  //         ...data.categories,
+  //         { id: uuidv4(), category, amount, type },
+  //       ];
+  //       const JSONcategories = JSON.stringify(updatedCategories);
 
-  // Checks loading state. Update to be skeleton loading
-  if (isLoading) {
-    return <LoadingSpinner isLoading={isLoading} />;
-  }
+  //       // const { data, error } = await supabase
+  //       //   .from("budget")
+  //       //   .update({ categories })
+  //       //   .eq("user_id", userId);
 
-  // Checks if budget exists & form closed. False - returns set up budget page
-  if (!data.budgetSetUp && !formOpen) {
-    return <SetUpMessage openForm={setFormOpen} />;
-  }
+  //       // console.log(data, error);
+  //     }
+  //   }
 
-  // Check to prevent mapping through undefined
-  if (formOpen) {
-    return (
-      <UpdateForm
-        formOpen={formOpen}
-        setFormOpen={setFormOpen}
-        onSubmit={handleAddCategory}
-      />
-    );
-  }
+  //   // Checks loading state. Update to be skeleton loading
+  //   if (isLoading) {
+  //     return <LoadingSpinner isLoading={isLoading} />;
+  //   }
 
-  const { budgetSetUp, categories } = data;
+  //   // Checks if budget exists & form closed. False - returns set up budget page
+  //   if (!data.budgetSetUp && !formOpen) {
+  //     return <SetUpMessage openForm={setFormOpen} />;
+  //   }
 
-  return (
-    <div className="flex flex-col items-center">
-      <BudgetCategoriesTotals categories={categories} />
-      <h2 className="h-[1px] bg-stone-500 w-11/12"></h2>
+  //   // Check to prevent mapping through undefined
+  //   if (formOpen) {
+  //     return (
+  //       <UpdateForm
+  //         formOpen={formOpen}
+  //         setFormOpen={setFormOpen}
+  //         onSubmit={handleAddCategory}
+  //       />
+  //     );
+  //   }
 
-      {/* UI layout for catgeories */}
-      <div className="w-full mt-6">
-        <BudgetCategoriesLayout
-          setUp={budgetSetUp}
-          categories={categories}
-          openForm={setFormOpen}
-        />
-      </div>
+  //   const { budgetSetUp, categories } = data;
 
-      {/* Shows different button once budget is setup */}
-      {!formOpen && budgetSetUp && (
-        <Button variant="contained" onClick={() => setFormOpen(true)}>
-          ADD
-        </Button>
-      )}
-    </div>
-  );
+  //   return (
+  //     <div className="flex flex-col items-center">
+  //       <BudgetCategoriesTotals categories={categories} />
+  //       <h2 className="h-[1px] bg-stone-500 w-11/12"></h2>
+
+  //       {/* UI layout for catgeories */}
+  //       <div className="w-full mt-6">
+  //         <BudgetCategoriesLayout
+  //           setUp={budgetSetUp}
+  //           categories={categories}
+  //           openForm={setFormOpen}
+  //         />
+  //       </div>
+
+  //       {/* Shows different button once budget is setup */}
+  //       {!formOpen && budgetSetUp && (
+  //         <Button variant="contained" onClick={() => setFormOpen(true)}>
+  //           ADD
+  //         </Button>
+  //       )}
+  //     </div>
+  //   );
 }
 
 export default Update;
