4143bf337de376768b7ab6f140073470df1fd849
JustinRivera-9 <justin_rivera9@outlook.com>
2024-01-14 13:44:46 -0700

    UI - fixed spacing issue

--- src/components/budget/BudgetGoalSummary.jsx
+++ src/components/budget/BudgetGoalSummary.jsx
@@ -11,22 +11,25 @@ function BudgetGoalSummary({ income, expenses }) {
   const net = totalIncome - totalExpenses;
 
   return (
-    <div className="flex my-4 px-6 py-2 justify-between items-center w-10/12 md:w-1/2 bg-[#404040] rounded-xl">
-      <div className="text-center">
-        <p className="text-green-300">Total Income</p>
-        <p>{formatCurrency(totalIncome)}</p>
+    <>
+      <div className="flex mx-auto my-2 px-6 py-2 justify-between items-center w-10/12 md:w-1/2 bg-[#404040] rounded-xl">
+        <div className="text-center">
+          <p className="text-green-300">Total Income</p>
+          <p>{formatCurrency(totalIncome)}</p>
+        </div>
+        <p className="text-2xl">-</p>
+        <div className="text-center">
+          <p className="text-red-300">Total Expenses</p>
+          <p>{formatCurrency(totalExpenses)}</p>
+        </div>
+        <p className="text-2xl">=</p>
+        <div className="text-center">
+          <p className="text-blue-500">Net Income</p>
+          <p>{formatCurrency(net)}</p>
+        </div>
       </div>
-      <p className="text-2xl">-</p>
-      <div className="text-center">
-        <p className="text-red-300">Total Expenses</p>
-        <p>{formatCurrency(totalExpenses)}</p>
-      </div>
-      <p className="text-2xl">=</p>
-      <div className="text-center">
-        <p className="text-blue-500">Net Income</p>
-        <p>{formatCurrency(net)}</p>
-      </div>
-    </div>
+      <h2 className="h-[1px] mx-auto bg-stone-500 w-11/12 my-4"></h2>
+    </>
   );
 }
 
