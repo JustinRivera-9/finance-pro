c9584467f9c10ed52ce761b4bac9fd1c40973b27
JustinRivera-9 <justin_rivera9@outlook.com>
2024-01-28 16:50:39 -0700

    Refactored form for React Hook Form. Still need validation

--- src/components/budget/PlannedForm.jsx
+++ src/components/budget/PlannedForm.jsx
@@ -1,54 +1,122 @@
-import { Backdrop, Button, TextField } from "@mui/material";
-import { useState } from "react";
-import SelectInput from "../../ui/SelectInput";
-import TextInput from "../../ui/TextInput";
+import {
+  Backdrop,
+  Button,
+  Checkbox,
+  FormControl,
+  FormControlLabel,
+  FormGroup,
+  InputLabel,
+  MenuItem,
+  OutlinedInput,
+  Select,
+  TextField,
+} from "@mui/material";
+import { useForm } from "react-hook-form";
+import { createNewArray } from "../../utils/helperFunctions";
 
 function PlannedForm({ formOpen, setFormOpen }) {
-  const [category, setCategory] = useState("");
-  const [amount, setAmount] = useState("");
-  const [type, setType] = useState("");
+  const {
+    register,
+    formState: { errors, dirtyFields },
+    handleSubmit,
+    reset,
+  } = useForm({
+    defaultValues: {
+      category: "",
+      amount: "",
+      type: "",
+      isFixed: false,
+      fixedDate: "",
+    },
+  });
 
-  function handleSubmit(e) {
-    e.preventDefault();
-    const formData = { category, amount, type };
+  const onError = (errors) => {
+    console.log(errors);
+  };
+
+  const onCancel = () => {
     setFormOpen(false);
-    resetForm();
+    reset();
+  };
 
-    // console.log(category, amount, type);
-  }
-
-  function handleCancel() {
+  const onSubmit = (data) => {
+    console.log(data);
     setFormOpen(false);
-    resetForm();
-  }
-
-  function resetForm() {
-    setCategory("");
-    setAmount("");
-    setType("Expense");
-  }
+    reset();
+  };
 
   return (
     <Backdrop open={formOpen}>
       <div className="flex flex-col bg-[#303030] p-8 rounded-xl">
-        <form className="flex flex-col" onSubmit={handleSubmit}>
-          <TextInput type="text" setValue={setCategory} isRequired={true}>
-            Category
-          </TextInput>
-          <TextInput type="number" setValue={setAmount} isRequired={true}>
-            Amount
-          </TextInput>
-          <SelectInput setValue={setType} options={["expense", "income"]}>
-            Type
-          </SelectInput>
-          <Button
-            variant="contained"
-            type="submit"
+        <form
+          className="flex flex-col"
+          onSubmit={handleSubmit(onSubmit, onError)}
+        >
+          {/* CATEGORY INPUT */}
+          <TextField
             sx={{ marginTop: "1.5rem" }}
-          >
+            id="outlined"
+            type="text"
+            label="Category"
+            required
+            {...register("category")}
+          />
+
+          {/* AMOUNT INPUT */}
+          <TextField
+            sx={{ marginTop: "1.5rem" }}
+            id="outlined"
+            type="number"
+            label="Amount"
+            required
+            {...register("amount")}
+          />
+
+          {/* TYPE INPUT */}
+          <FormControl required sx={{ marginTop: "1.5rem" }}>
+            <InputLabel id="type">Type</InputLabel>
+            <Select
+              id="type"
+              input={<OutlinedInput label="Type" id="type" />}
+              {...register("type")}
+            >
+              <MenuItem value="income">Income</MenuItem>
+              <MenuItem value="expense">Expense</MenuItem>
+            </Select>
+          </FormControl>
+
+          {/* IS FIXED CHECKBOX */}
+          {dirtyFields.type && (
+            <FormGroup sx={{ marginTop: "0.75rem" }}>
+              <FormControlLabel
+                control={<Checkbox size="medium" {...register("isFixed")} />}
+                label="Is this a fixed expense?"
+              />
+            </FormGroup>
+          )}
+
+          {/* FIXED DATE INPUT */}
+          {dirtyFields.isFixed && (
+            <FormControl required sx={{ marginTop: "0.75rem" }}>
+              <InputLabel id="fixedDate">Day of month</InputLabel>
+              <Select
+                id="fixedDate"
+                input={<OutlinedInput label="Day of month" id="fixedDate" />}
+                {...register("fixedDate")}
+              >
+                {createNewArray(32).map((option) => (
+                  <MenuItem key={option} value={option}>
+                    {option}
+                  </MenuItem>
+                ))}
+              </Select>
+            </FormControl>
+          )}
+
+          <Button variant="contained" type="submit" sx={{ marginTop: "1rem" }}>
             ADD
           </Button>
-          <Button sx={{ marginTop: ".75rem" }} onClick={handleCancel}>
+          <Button onClick={onCancel} sx={{ marginTop: ".75rem" }}>
             Cancel
           </Button>
         </form>
