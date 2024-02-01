2bfc3f78888b62a3201c41be32180affe3106e64
JustinRivera-9 <justin_rivera9@outlook.com>
2024-01-31 23:00:31 -0700

    Updated message to get started

--- src/components/budget/SetUpMessage.jsx
+++ src/components/budget/SetUpMessage.jsx
@@ -3,9 +3,12 @@ import { Button } from "@mui/material";
 function SetUpMessage({ openForm }) {
   return (
     <div className="space-y-4 text-center">
-      <h1 className="text-3xl">
-        Set up your budget to start tracking expenses!
-      </h1>
+      <p className="text-3xl text-[#48ff00]">
+        Customize your budget categories!
+      </p>
+      <p className="text-2xl">
+        Try starting with your income or your fixed monthly expenses!
+      </p>
       <Button onClick={() => openForm(true)} variant="contained">
         Set Up
       </Button>
