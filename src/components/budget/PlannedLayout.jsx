f85368df7aaa7e35deca5829f64e5732651b5ad1
JustinRivera-9 <justin_rivera9@outlook.com>
2024-01-31 22:46:28 -0700

    Added fixed expenses prop to PlannedSummary

--- src/components/budget/PlannedLayout.jsx
+++ src/components/budget/PlannedLayout.jsx
@@ -13,15 +13,19 @@ function PlannedLayout({ categories, openForm }) {
 
   const incomeArr = plannedCategories.filter((el) => el.type === "income");
   const expenseArr = plannedCategories.filter(
-    (el) => el.type === "expense" && el.isFixed === "false"
+    (el) => el.type === "expense" && !el.isFixed
   );
   const fixedExpenseArr = plannedCategories.filter(
-    (el) => el.type === "expense" && el.isFixed === "true"
+    (el) => el.type === "expense" && el.isFixed
   );
 
   return (
     <div className="flex flex-col mx-auto">
-      <PlannedSummary income={incomeArr} expenses={expenseArr} />
+      <PlannedSummary
+        income={incomeArr}
+        expenses={expenseArr}
+        fixed={fixedExpenseArr}
+      />
       <div className="flex flex-col sm:flex-row sm:justify-around px-4">
         <div className="sm:w-[30%]">
           <h2 className="w-fit mx-auto text-stone-700 text-xl font-semibold bg-green-400 rounded-full py-2 px-14">
