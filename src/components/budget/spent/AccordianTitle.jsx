9862c7d09881ed5c7a527e5afbd1007f90448c3e
JustinRivera-9 <justin_rivera9@outlook.com>
2024-02-04 18:04:46 -0700

    New component to make CategoryAccordian.jsx more readable

---
+++ src/components/budget/spent/AccordianTitle.jsx
@@ -0,0 +1,34 @@
+import AddCircleIcon from "@mui/icons-material/AddCircle";
+import RemoveCircleIcon from "@mui/icons-material/RemoveCircle";
+
+import { formatCurrency } from "../../../utils/helperFunctions";
+
+function CategoryTitle({ info, isOpen, setIsOpen }) {
+  const { plannedAmount, spentAmount, categoryName } = info;
+  const percentSpent = (spentAmount / plannedAmount) * 100;
+
+  return (
+    <div className="flex justify-between w-full">
+      <div className="flex justify-around items-baseline space-x-4">
+        <p className="text-2xl pl-4 capitalize">{categoryName}</p>
+        <p
+          style={{
+            color: percentSpent > 100 ? "#D22B2B" : `#48ff00`,
+          }}
+          className="text-[#48ff00c2] text-xl"
+        >
+          Spent: {formatCurrency(spentAmount)}
+        </p>
+      </div>
+      <button onClick={() => setIsOpen((show) => !show)} className="pr-4">
+        {isOpen ? (
+          <RemoveCircleIcon color="primary" />
+        ) : (
+          <AddCircleIcon color="primary" />
+        )}
+      </button>
+    </div>
+  );
+}
+
+export default CategoryTitle;
