0a787ddef623279adf05b3eb07ef6c7f7ec4f238
JustinRivera-9 <justin_rivera9@outlook.com>
2024-02-04 18:06:06 -0700

    Refactored and split into more components

--- src/components/budget/spent/CategoryAccordian.jsx
+++ src/components/budget/spent/CategoryAccordian.jsx
@@ -3,8 +3,10 @@ import RemoveCircleIcon from "@mui/icons-material/RemoveCircle";
 
 import { useState } from "react";
 
+import CategoryProgressBar from "./AccordianProgressBar";
 import { formatCurrency } from "../../../utils/helperFunctions";
 import ExpenseCard from "./ExpenseCard";
+import CategoryTitle from "./AccordianTitle";
 
 function CategoryAccordian({ category }) {
   const [isOpen, setIsOpen] = useState(false);
@@ -17,27 +19,8 @@ function CategoryAccordian({ category }) {
 
   return (
     <li className="flex flex-col space-y-2 py-4 items-center bg-[#404040] rounded-xl">
-      <div className="flex justify-between w-full">
-        <div className="flex justify-around items-baseline space-x-4">
-          <p className="text-2xl pl-4 capitalize">{categoryName}</p>
-          <p className="text-[#48ff00c2] text-xl">
-            Spent: {formatCurrency(spentAmount)}
-          </p>
-        </div>
-        <button onClick={() => setIsOpen((show) => !show)} className="pr-4">
-          {isOpen ? (
-            <RemoveCircleIcon color="primary" />
-          ) : (
-            <AddCircleIcon color="primary" />
-          )}
-        </button>
-      </div>
-      <div className="flex justify-around items-center w-full">
-        <div className="h-5 bg-[#48ff00] rounded-xl w-4/5"></div>
-        <p className="pr-2 text-md text-slate-300">
-          {formatCurrency(plannedAmount)}
-        </p>
-      </div>
+      <CategoryTitle info={category} isOpen={isOpen} setIsOpen={setIsOpen} />
+      <CategoryProgressBar planned={plannedAmount} spent={spentAmount} />
       {isOpen && (
         <ul className="flex flex-col space-y-2 w-full px-4">
           {expenses.map((el) => (
