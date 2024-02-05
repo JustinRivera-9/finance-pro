f9502ed78681c7249d30af73ce234cf13df05825
JustinRivera-9 <justin_rivera9@outlook.com>
2024-02-04 18:07:48 -0700

    Removed files & variables no longer being used

--- src/components/budget/spent/CategoryAccordian.jsx
+++ src/components/budget/spent/CategoryAccordian.jsx
@@ -1,21 +1,12 @@
-import AddCircleIcon from "@mui/icons-material/AddCircle";
-import RemoveCircleIcon from "@mui/icons-material/RemoveCircle";
-
 import { useState } from "react";
 
 import CategoryProgressBar from "./AccordianProgressBar";
-import { formatCurrency } from "../../../utils/helperFunctions";
 import ExpenseCard from "./ExpenseCard";
 import CategoryTitle from "./AccordianTitle";
 
 function CategoryAccordian({ category }) {
   const [isOpen, setIsOpen] = useState(false);
-  const {
-    categoryName,
-    plannedAmount,
-    spentAmount,
-    expenses2024: expenses,
-  } = category;
+  const { plannedAmount, spentAmount, expenses2024: expenses } = category;
 
   return (
     <li className="flex flex-col space-y-2 py-4 items-center bg-[#404040] rounded-xl">
