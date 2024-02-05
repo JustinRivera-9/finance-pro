0ada4d375644d09185732997b16230eb9a17ee16
JustinRivera-9 <justin_rivera9@outlook.com>
2024-02-04 17:32:51 -0700

    Removed test data

--- src/pages/app/budget/Spent.jsx
+++ src/pages/app/budget/Spent.jsx
@@ -8,59 +8,6 @@ import { getExpenses } from "../../../services/apiExpenses";
 import LoadingSpinner from "../../../ui/LoadingSpinner";
 import { expenseArrayMutation } from "../../../utils/helperFunctions";
 
-const tempData = [
-  {
-    categoryName: "Date Night",
-    plannedAmount: 250,
-    spentAmount: 105.35,
-    expenses2024: [
-      {
-        id: 1,
-        date: "Jan 18",
-        amount: 34.56,
-        description: "Pizza night at pietras",
-      },
-      {
-        id: 2,
-        date: "Jan 6",
-        amount: 54.21,
-        description: "Mini golf at PuttPutt",
-      },
-      {
-        id: 3,
-        date: "Jan 25",
-        amount: 75.94,
-        description: "Dinner at Your Neighbor Felix",
-      },
-    ],
-  },
-  {
-    categoryName: "Beer",
-    plannedAmount: 125,
-    spentAmount: 24.56,
-    expenses2024: [
-      {
-        id: 1,
-        date: "Jan 9",
-        amount: 38.45,
-        description: "Denver beer co",
-      },
-      {
-        id: 2,
-        date: "Jan 6",
-        amount: 54.21,
-        description: "Mini golf at PuttPutt",
-      },
-    ],
-  },
-  {
-    categoryName: "Zoe",
-    plannedAmount: 60,
-    spentAmount: 0,
-    expenses2024: [],
-  },
-];
-
 function Spent() {
   const userId = useContext(AuthContext);
 
