24745e4ed28b23e1e9ae2142baeb7f74c53a90a6
JustinRivera-9 <justin_rivera9@outlook.com>
2024-02-09 22:31:29 -0700

    Bug - Passing entire expense object to MenuItem ILO just the expenseID

--- src/components/budget/spent/ExpenseCard.jsx
+++ src/components/budget/spent/ExpenseCard.jsx
@@ -5,7 +5,7 @@ import { formatCurrency } from "../../../utils/helperFunctions";
 function ExpenseCard({ expense }) {
   const { id, amount, date, description } = expense;
   const [month, day] = date.split(" ");
-
+  console.log(expense);
   return (
     <li className="flex mt-2 px-3 py-1 justify-between bg-[#292929] items-center w-full rounded-2xl">
       <div className="flex space-x-4 items-center">
@@ -17,7 +17,7 @@ function ExpenseCard({ expense }) {
       </div>
       <div className="flex items-center space-x-4">
         <p className="text-[#48ff00bd] text-xl">{formatCurrency(amount)}</p>
-        <MenuIcon expenseId={id} />
+        <MenuIcon expense={expense} />
         {/* <MoreVertIcon /> */}
       </div>
     </li>
