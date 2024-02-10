4643f59ef0c1ec1a29e823635b4af7801349d050
JustinRivera-9 <justin_rivera9@outlook.com>
2024-02-09 22:33:15 -0700

    Feature - Now recognizes edit actions and passes expense obj to form

--- src/ui/MenuIcon.jsx
+++ src/ui/MenuIcon.jsx
@@ -1,60 +1,78 @@
-import * as React from "react";
 import IconButton from "@mui/material/IconButton";
 import Menu from "@mui/material/Menu";
 import MenuItem from "@mui/material/MenuItem";
 import MoreVertIcon from "@mui/icons-material/MoreVert";
 
+import { useState } from "react";
+import ExpenseForm from "../components/budget/spent/ExpenseForm";
+
 const options = ["Edit", "Delete"];
 
 const ITEM_HEIGHT = 48;
 
-export default function MenuIcon() {
-  const [anchorEl, setAnchorEl] = React.useState(null);
+export default function MenuIcon({ expense }) {
+  console.log(expense);
+  const [formOpen, setFormOpen] = useState(false);
+
+  const [anchorEl, setAnchorEl] = useState(null);
   const open = Boolean(anchorEl);
   const handleClick = (event) => {
     setAnchorEl(event.currentTarget);
   };
-  const handleClose = () => {
+
+  const handleClose = (e) => {
     setAnchorEl(null);
+    if (e.target.tabIndex === 0) setFormOpen(true);
+    if (e.target.tabIndex === -1)
+      console.log("Delete button clicked", expense.id);
   };
 
   return (
-    <div>
-      <IconButton
-        aria-label="more"
-        id="long-button"
-        aria-controls={open ? "long-menu" : undefined}
-        aria-expanded={open ? "true" : undefined}
-        aria-haspopup="true"
-        onClick={handleClick}
-      >
-        <MoreVertIcon />
-      </IconButton>
-      <Menu
-        id="long-menu"
-        MenuListProps={{
-          "aria-labelledby": "long-button",
-        }}
-        anchorEl={anchorEl}
-        open={open}
-        onClose={handleClose}
-        PaperProps={{
-          style: {
-            maxHeight: ITEM_HEIGHT * 4.5,
-            width: "20ch",
-          },
-        }}
-      >
-        {options.map((option) => (
-          <MenuItem
-            key={option}
-            selected={option === "Pyxis"}
-            onClick={handleClose}
-          >
-            {option}
-          </MenuItem>
-        ))}
-      </Menu>
-    </div>
+    <>
+      <div>
+        <IconButton
+          aria-label="more"
+          id="long-button"
+          aria-controls={open ? "long-menu" : undefined}
+          aria-expanded={open ? "true" : undefined}
+          aria-haspopup="true"
+          onClick={handleClick}
+        >
+          <MoreVertIcon />
+        </IconButton>
+        <Menu
+          id="long-menu"
+          MenuListProps={{
+            "aria-labelledby": "long-button",
+          }}
+          anchorEl={anchorEl}
+          open={open}
+          onClose={handleClose}
+          PaperProps={{
+            style: {
+              maxHeight: ITEM_HEIGHT * 4.5,
+              width: "20ch",
+            },
+          }}
+        >
+          {options.map((option) => (
+            <MenuItem
+              key={option}
+              selected={option === "Pyxis"}
+              onClick={handleClose}
+            >
+              {option}
+            </MenuItem>
+          ))}
+        </Menu>
+      </div>
+      {formOpen && (
+        <ExpenseForm
+          formOpen={formOpen}
+          setFormOpen={setFormOpen}
+          expenseToEdit={expense}
+        />
+      )}
+    </>
   );
 }
