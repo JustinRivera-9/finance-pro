ab447de97a3ed6c2a3f4e4f4908e5ea14b3e74f1
JustinRivera-9 <justin_rivera9@outlook.com>
2024-02-09 22:45:32 -0700

    General - Removed console.logs

--- src/components/budget/spent/ExpenseCard.jsx
+++ src/components/budget/spent/ExpenseCard.jsx
@@ -1,11 +1,10 @@
-import MenuIcon from "../../../ui/MenuIcon";
-
 import { formatCurrency } from "../../../utils/helperFunctions";
+import ExpenseCardActions from "./ExpenseCardActions";
 
 function ExpenseCard({ expense }) {
   const { id, amount, date, description } = expense;
   const [month, day] = date.split(" ");
-  console.log(expense);
+
   return (
     <li className="flex mt-2 px-3 py-1 justify-between bg-[#292929] items-center w-full rounded-2xl">
       <div className="flex space-x-4 items-center">
@@ -17,7 +16,7 @@ function ExpenseCard({ expense }) {
       </div>
       <div className="flex items-center space-x-4">
         <p className="text-[#48ff00bd] text-xl">{formatCurrency(amount)}</p>
-        <MenuIcon expense={expense} />
+        <ExpenseCardActions expense={expense} />
         {/* <MoreVertIcon /> */}
       </div>
     </li>
