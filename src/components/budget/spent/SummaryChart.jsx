0bed2eaa52014c81ddf0ec8ba030b38a47e72a2a
JustinRivera-9 <justin_rivera9@outlook.com>
2024-02-11 10:52:20 -0700

    init

---
+++ src/components/budget/spent/SummaryChart.jsx
@@ -0,0 +1,15 @@
+function SummaryChart({ expenses }) {
+  console.log(expenses);
+
+  return (
+    <div className="flex justify-between">
+      {/* PIE CHART */}
+      <p>SUMMARY CHART</p>
+
+      {/* CHART LEGEND */}
+      <p>Chart Legend</p>
+    </div>
+  );
+}
+
+export default SummaryChart;
