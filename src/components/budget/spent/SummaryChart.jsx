b41b1522741cb17e61aa1154e01d58fcbbb9a3e8
JustinRivera-9 <justin_rivera9@outlook.com>
2024-02-11 11:59:32 -0700

    Feature - Basic pie chart created

--- src/components/budget/spent/SummaryChart.jsx
+++ src/components/budget/spent/SummaryChart.jsx
@@ -1,13 +1,13 @@
+import { PieChart } from "@mui/x-charts";
+import { prepareSpentChartData } from "../../../utils/helperFunctions";
+
 function SummaryChart({ expenses }) {
-  console.log(expenses);
+  const chartData = [{ data: prepareSpentChartData(expenses) }];
+  console.log(chartData);
 
   return (
     <div className="flex justify-between">
-      {/* PIE CHART */}
-      <p>SUMMARY CHART</p>
-
-      {/* CHART LEGEND */}
-      <p>Chart Legend</p>
+      <PieChart series={chartData} width={400} height={200} />
     </div>
   );
 }
