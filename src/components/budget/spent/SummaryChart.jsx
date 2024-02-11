0964ca5756b02a628db8006cc0c45ba0d2a746f0
JustinRivera-9 <justin_rivera9@outlook.com>
2024-02-11 13:53:30 -0700

    Feature - Pie chart complete

--- src/components/budget/spent/SummaryChart.jsx
+++ src/components/budget/spent/SummaryChart.jsx
@@ -1,13 +1,40 @@
-import { PieChart } from "@mui/x-charts";
-import { prepareSpentChartData } from "../../../utils/helperFunctions";
+import { PieChart, pieArcLabelClasses } from "@mui/x-charts";
+import {
+  formatCurrency,
+  prepareSpentChartData,
+} from "../../../utils/helperFunctions";
 
 function SummaryChart({ expenses }) {
-  const chartData = [{ data: prepareSpentChartData(expenses) }];
-  console.log(chartData);
+  const chartData = prepareSpentChartData(expenses);
 
   return (
-    <div className="flex justify-between">
-      <PieChart series={chartData} width={400} height={200} />
+    <div className="flex">
+      <PieChart
+        series={[
+          {
+            data: chartData,
+            highlightScope: { faded: "global", highlighted: "category" },
+            faded: { innerRadius: 20, additionalRadius: -20 },
+            // arcLabel: (item) => `${formatCurrency(item.value)}`,
+            arcLabel: (item) => `${item.label.split(" ")[0]}`,
+            arcLabelMinAngle: 30,
+            innerRadius: 20,
+            paddingAngle: 3,
+            cornerRadius: 3,
+            cx: 150,
+          },
+        ]}
+        sx={{
+          [`& .${pieArcLabelClasses.root}`]: {
+            fill: "black",
+            fontWeight: "bold",
+            fontSize: "0.75rem",
+            textWrap: "wrap",
+          },
+        }}
+        width={475}
+        height={250}
+      />
     </div>
   );
 }
