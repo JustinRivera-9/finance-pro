d943784c68e1d1b99d946b202183e3a0c1dafd3d
JustinRivera-9 <justin_rivera9@outlook.com>
2024-02-11 19:44:11 -0700

    Month filter button finished

--- src/components/budget/spent/MonthFilterButton.jsx
+++ src/components/budget/spent/MonthFilterButton.jsx
@@ -1,8 +1,32 @@
-function MonthFilterButton() {
+import { FormControl, InputLabel, MenuItem, Select } from "@mui/material";
+import { useState } from "react";
+
+function MonthFilterButton({ monthFilter, setMonthFilter }) {
   return (
-    <div>
-      <p>January 2024</p>
-    </div>
+    <Select
+      variant="standard"
+      value={monthFilter}
+      onChange={(e) => setMonthFilter(e.target.value)}
+      sx={{
+        width: "fit-content",
+        margin: "0.75rem auto",
+        padding: "0.25rem",
+        color: "#48ff00",
+      }}
+    >
+      <MenuItem value="Jan">January</MenuItem>
+      <MenuItem value="Feb">February</MenuItem>
+      <MenuItem value="Mar">March</MenuItem>
+      <MenuItem value="Apr">April</MenuItem>
+      <MenuItem value="May">May</MenuItem>
+      <MenuItem value="Jun">June</MenuItem>
+      <MenuItem value="Jul">July</MenuItem>
+      <MenuItem value="Aug">August</MenuItem>
+      <MenuItem value="Sep">September</MenuItem>
+      <MenuItem value="Oct">October</MenuItem>
+      <MenuItem value="Nov">November</MenuItem>
+      <MenuItem value="Dec">December</MenuItem>
+    </Select>
   );
 }
 
