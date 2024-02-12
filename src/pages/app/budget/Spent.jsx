38a1bbb7ee319d947071646b285302bc3c855aa6
JustinRivera-9 <justin_rivera9@outlook.com>
2024-02-11 19:59:30 -0700

    Feature - filter button now works

--- src/pages/app/budget/Spent.jsx
+++ src/pages/app/budget/Spent.jsx
@@ -1,7 +1,7 @@
 import CategoryList from "../../../components/budget/spent/CategoryList";
 
 import { useQuery } from "@tanstack/react-query";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { AuthContext } from "../../../utils/context";
 
 import SetUpMessage from "../../../components/budget/spent/SetUpMessage";
@@ -10,14 +10,16 @@ import LoadingSpinner from "../../../ui/LoadingSpinner";
 import SummaryPieChart from "../../../components/budget/spent/SummaryPieChart";
 import MonthFilterButton from "../../../components/budget/spent/MonthFilterButton";
 import SummaryBarChart from "../../../components/budget/spent/SummaryBarChart";
+import { monthFilterExpenseData } from "../../../utils/helperFunctions";
 
 function Spent() {
+  const [monthFilter, setMonthFilter] = useState("Feb");
   const userId = useContext(AuthContext);
 
   // QUERY SET UP
   const { data, isLoading, error } = useQuery({
-    queryKey: ["expenses", userId],
-    queryFn: () => getExpenses(userId),
+    queryKey: ["expenses", userId, monthFilter],
+    queryFn: () => getExpenses(userId, monthFilter),
   });
 
   if (isLoading) {
@@ -29,13 +31,17 @@ function Spent() {
     return <h2>There was an error in Spent Page</h2>;
   }
 
-  const { expenses } = data;
+  const { expenses: fullExpenses } = data;
+  const expenses = monthFilterExpenseData(fullExpenses, monthFilter);
 
   return (
     <div className="flex flex-col space-y-4 w-11/12 mx-auto text-center">
-      {expenses.length ? (
+      {fullExpenses.length ? (
         <>
-          <MonthFilterButton />
+          <MonthFilterButton
+            monthFilter={monthFilter}
+            setMonthFilter={setMonthFilter}
+          />
           <div className="flex flex-col space-y-10 md:space-y-0 md:w-full md:flex-row md:justify-between mx-auto">
             <SummaryPieChart expenses={expenses} />
             <SummaryBarChart expenses={expenses} />
