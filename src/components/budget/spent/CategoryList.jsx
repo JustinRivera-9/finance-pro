d66cbcebf2291cf02f14b54d0697bd081216810c
JustinRivera-9 <justin_rivera9@outlook.com>
2024-02-12 21:21:56 -0700

    UI - updated for large screens

--- src/components/budget/spent/CategoryList.jsx
+++ src/components/budget/spent/CategoryList.jsx
@@ -2,7 +2,7 @@ import CategoryAccordian from "./CategoryAccordian";
 
 function CategoryList({ categories }) {
   return (
-    <ul className="flex flex-col space-y-4">
+    <ul className="flex flex-col space-y-4 md:flex-row md:flex-wrap md:justify-center md:space-x-4 md:w-full">
       {categories.map((el) => (
         <CategoryAccordian key={el.categoryName} category={el} />
       ))}
