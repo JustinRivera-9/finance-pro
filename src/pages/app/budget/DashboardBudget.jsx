094f93b9c7e111a7b173a97f614dc7eb875f8654
JustinRivera-9 <justin_rivera9@outlook.com>
2024-02-13 21:27:18 -0700

    Set up react query and passed data via expenses prop

---
+++ src/pages/app/budget/DashboardBudget.jsx
@@ -0,0 +1,39 @@
+import BudgetTotals from "../../../components/budget/dashboard/BudgetTotals";
+import CategorySummaryChart from "../../../components/budget/dashboard/CategorySummaryChart";
+import MonthSummaryChart from "../../../components/budget/dashboard/MonthSummaryChart";
+import UpcomingFixedExpenses from "../../../components/budget/dashboard/UpcomingFixedExpenses";
+import LoadingSpinner from "../../../ui/LoadingSpinner";
+
+import { getExpenses } from "../../../services/apiExpenses";
+import { useContext } from "react";
+import { AuthContext } from "../../../utils/context";
+import { useQuery } from "@tanstack/react-query";
+
+function DashboardBudget() {
+  const userId = useContext(AuthContext);
+  const { data, isLoading, error } = useQuery({
+    queryKey: ["expenses", userId],
+    queryFn: () => getExpenses(userId),
+  });
+
+  if (isLoading) {
+    return <LoadingSpinner isLoading={isLoading} />;
+  }
+
+  if (error) {
+    return <h2>There was an error: {error.message}</h2>;
+  }
+
+  const expenses = data.expenses;
+
+  return (
+    <div className="flex flex-col justify-center mx-auto mt-4 w-11/12 space-y-4 md:flex-row md:flex-wrap md:space-y-0 ">
+      <BudgetTotals expenses={expenses} />
+      <MonthSummaryChart expenses={expenses} />
+      <UpcomingFixedExpenses expenses={expenses} />
+      <CategorySummaryChart expenses={expenses} />
+    </div>
+  );
+}
+
+export default DashboardBudget;
