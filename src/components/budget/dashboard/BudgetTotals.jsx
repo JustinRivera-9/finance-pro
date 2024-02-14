a3fd98f7f17ae54db12a1867b46c82505a3bd6bb
JustinRivera-9 <justin_rivera9@outlook.com>
2024-02-13 20:58:05 -0700

    Finished computing totals

--- src/components/budget/dashboard/BudgetTotals.jsx
+++ src/components/budget/dashboard/BudgetTotals.jsx
@@ -1,11 +1,28 @@
+import {
+  formatCurrency,
+  prepareAverageSpend,
+  reduceArr,
+} from "../../../utils/helperFunctions";
 import BudgetTotalsCard from "./BudgetTotalsCard";
 
-function BudgetTotals() {
+function BudgetTotals({ expenses }) {
+  const monthlyBudget = reduceArr(
+    expenses.map((el) => {
+      return { amount: el.plannedAmount };
+    })
+  );
+
+  const averageSpend = prepareAverageSpend(expenses);
+
   return (
-    <div className="flex justify-between w-full mx-auto text-center bg-[#505050] p-4 rounded-xl md:w-fit md:space-x-4 md:h-fit">
-      <BudgetTotalsCard amount="$1,875.00">Planned Spend</BudgetTotalsCard>
-      <BudgetTotalsCard amount="$1,875.00">Actual Spend</BudgetTotalsCard>
-      <BudgetTotalsCard amount="$1,875.00">Net Cash Flow</BudgetTotalsCard>
+    <div className="flex justify-around w-full mx-auto text-center p-4 rounded-xl md:w-fit md:space-x-4 md:h-fit">
+      <BudgetTotalsCard amount={formatCurrency(monthlyBudget)}>
+        Monthly Budget
+      </BudgetTotalsCard>
+      <BudgetTotalsCard amount={formatCurrency(averageSpend)}>
+        Average Spend
+      </BudgetTotalsCard>
+      {/* <BudgetTotalsCard amount="$1,875.00">Net Cash Flow</BudgetTotalsCard> */}
     </div>
   );
 }
