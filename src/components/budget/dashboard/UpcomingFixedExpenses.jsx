c2768284d7933472578dff027e70d90f7ae5008d
JustinRivera-9 <justin_rivera9@outlook.com>
2024-02-13 21:01:31 -0700

    Fetaure - updated temp data with real data

--- src/components/budget/dashboard/UpcomingFixedExpenses.jsx
+++ src/components/budget/dashboard/UpcomingFixedExpenses.jsx
@@ -1,23 +1,21 @@
 import { formatCurrency } from "../../../utils/helperFunctions";
 
-function UpcomingFixedExpenses() {
-  const tempData = [
-    { categoryName: "Mortgage", amount: 1400, date: "Feb 1" },
-    { categoryName: "Car", amount: 395, date: "Feb 5" },
-    { categoryName: "Utilities", amount: 125, date: "Feb 14" },
-    { categoryName: "Student", amount: 158, date: "Feb 28" },
-  ];
+function UpcomingFixedExpenses({ expenses }) {
+  const fixedExpenses = expenses.filter((el) => el.isFixed);
+
   return (
     <div className="flex flex-col bg-[#505050] rounded-xl p-4 md:w-1/3 md:h-fit md:mt-4">
       <p className="text-slate-200 mb-4 text-center text-3xl">
         Upcoming Fixed Expenses
       </p>
       <div className="flex justify-between bg-[#505050] rounded-xl text-xl text-center">
-        {tempData.map((item) => (
+        {fixedExpenses.map((item) => (
           <div key={item.categoryName} className="bg-[#292929] p-2 rounded-xl">
             <p className="text-[#48ff00]">{item.date}</p>
             <p className="text-xl text-slate-300">{item.categoryName}</p>
-            <p className="text-slate-200">{formatCurrency(item.amount)}</p>
+            <p className="text-slate-200">
+              {formatCurrency(item.plannedAmount)}
+            </p>
           </div>
         ))}
       </div>
