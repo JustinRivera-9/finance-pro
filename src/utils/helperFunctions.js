ab090764f6c423242c8dc96636966ffb05405626
JustinRivera-9 <justin_rivera9@outlook.com>
2024-02-13 21:26:39 -0700

    Added function for preparing expenses by month

--- src/utils/helperFunctions.js
+++ src/utils/helperFunctions.js
@@ -189,3 +189,29 @@ export const prepareAverageSpend = (expenses) => {
 
   return averageSpend;
 };
+
+export const prepareExpensesByMonth = (expenses) => {
+  // Returns array of objects for all expenses
+  const allExpenses = expenses.flatMap((el) => el.expenses2024);
+
+  // Returns an array of objects containing only the Month and amount
+  const expensesByMonth = allExpenses.map((el) => {
+    return { month: el.date.split(" ")[0], amount: +el.amount };
+  });
+
+  // Returns object containing all expenses grouped by month
+  const finalArr = Object.groupBy(expensesByMonth, (expense) => expense.month);
+
+  // Converts object to array of array and reduces the array to a single value
+  const arrKeys = Object.entries(finalArr);
+  arrKeys.forEach((el) => {
+    return (el[1] = reduceArr(el[1]));
+  });
+
+  const monthExpenses = arrKeys.map((el) => {
+    const [month, amount] = el;
+    return { month, amount: +amount.toFixed(2) };
+  });
+
+  return monthExpenses;
+};
