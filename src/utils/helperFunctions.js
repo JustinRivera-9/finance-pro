9738fb47aa51ea2fd1b0647a4cb085a8dc777947
JustinRivera-9 <justin_rivera9@outlook.com>
2024-02-17 13:08:29 -0700

    Updated formatCurrency function for rounding

--- src/utils/helperFunctions.js
+++ src/utils/helperFunctions.js
@@ -2,9 +2,9 @@ import { v4 as uuidv4 } from "uuid";
 
 export const capitalize = (str) => str.charAt(0).toUpperCase() + str.slice(1);
 
-export const formatCurrency = (value, rounded = false) => {
+export const formatCurrency = (value, rounded) => {
   if (rounded) {
-    const number = +value;
+    const number = Math.round(+value);
 
     return new Intl.NumberFormat("en", {
       style: "currency",
