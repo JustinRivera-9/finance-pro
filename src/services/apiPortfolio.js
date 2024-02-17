942788cbe2e00ce0303298719dabe2c1d47350d1
JustinRivera-9 <justin_rivera9@outlook.com>
2024-02-17 01:45:47 -0700

    reading from database

--- src/services/apiPortfolio.js
+++ src/services/apiPortfolio.js
@@ -1,13 +1,17 @@
-import { supabase } from "@supabase/auth-ui-shared";
+import supabase from "./supabase";
 
 export const getPortfolio = async (userId) => {
-  console.log(userId);
-
   let { data: portfolio, error } = await supabase
     .from("portfolio")
-    .select("some_column,other_column");
+    .select("portfolio")
+    .eq("user_id", userId);
 
-  if (error) throw new Error(error.message);
+  if (error) {
+    console.log(error);
+    throw new Error(
+      "Portfolio could not be loaded. Please refresh and try again."
+    );
+  }
 
-  return portfolio;
+  return portfolio[0].portfolio;
 };
