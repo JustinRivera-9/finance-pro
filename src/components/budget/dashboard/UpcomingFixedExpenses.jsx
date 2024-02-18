c3ebbcd7509a1efe35a4e857e2ecfe77c5abfcfb
JustinRivera-9 <justin_rivera9@outlook.com>
2024-02-18 00:59:28 -0700

    UI - Fiexed layout issue of cards

--- src/components/budget/dashboard/UpcomingFixedExpenses.jsx
+++ src/components/budget/dashboard/UpcomingFixedExpenses.jsx
@@ -2,21 +2,24 @@ import { formatCurrency } from "../../../utils/helperFunctions";
 
 function UpcomingFixedExpenses({ expenses }) {
   const fixedExpenses = expenses.filter((el) => el.isFixed);
-
+  console.log(fixedExpenses);
   return (
-    <div className="flex flex-col bg-[#505050] rounded-xl p-4 md:w-1/3 md:h-fit md:mt-4">
+    <div className="flex flex-col bg-[#505050] rounded-xl p-2 md:w-1/3 md:h-fit md:mt-4">
       <p className="text-slate-200 mb-4 text-center text-3xl">
         Upcoming Fixed Expenses
       </p>
-      <div className="flex justify-between bg-[#505050] rounded-xl text-xl text-center">
+      <div className="flex flex-wrap justify-around bg-[#505050] rounded-xl text-xl text-center ">
         {fixedExpenses.map((item) => (
-          <div key={item.categoryName} className="bg-[#292929] p-2 rounded-xl">
-            <p className="text-[#48ff00]">{item.date}</p>
-            <p className="text-xl text-slate-300">{item.categoryName}</p>
-            <p className="text-slate-200">
-              {formatCurrency(item.plannedAmount)}
-            </p>
-          </div>
+          <ul
+            key={item.categoryName}
+            className="bg-[#292929] px-4 py-2 rounded-xl w-[48%] my-1"
+          >
+            <li className="text-[#48ff00]">{item.date}</li>
+            <li className="text-lg text-slate-300">{item.categoryName}</li>
+            <li className="text-slate-200">
+              {formatCurrency(item.plannedAmount, true)}
+            </li>
+          </ul>
         ))}
       </div>
     </div>
