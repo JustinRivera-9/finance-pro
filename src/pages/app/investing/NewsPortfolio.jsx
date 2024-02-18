c34e8f6124a4a3637aabf7e936e219561b05f867
JustinRivera-9 <justin_rivera9@outlook.com>
2024-02-18 00:20:23 -0700

    Ready for demo - Made coming soon copmonent

--- src/pages/app/investing/NewsPortfolio.jsx
+++ src/pages/app/investing/NewsPortfolio.jsx
@@ -1,7 +1,12 @@
 function NewsPortfolio() {
   return (
-    <div className="flex justify-center text-3xl mt-24">
-      <h1>INSERT PORTFOLIO NEWS PAGE</h1>
+    <div className="flex flex-col space-y-6 items-center mx-auto text-center p-6 w-3/4 bg-[#494949] rounded-lg mt-8">
+      <h2 className="text-3xl font-bold text-slate-300">Coming Soon!</h2>
+      <p className="text-xl text-slate-300">
+        This upcoming feature will allow you to control what stocks you want to
+        see news for over the last 30 days.
+      </p>
+      <p className="text-lg text-slate-300">Stay tuned for updates!</p>
     </div>
   );
 }
