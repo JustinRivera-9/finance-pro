95b70039d04c04d557638a8e9d91bf2cd020f866
JustinRivera-9 <justin_rivera9@outlook.com>
2024-02-18 00:59:40 -0700

    UI - Fixed layout issue with cards

--- src/components/budget/dashboard/BudgetTotals.jsx
+++ src/components/budget/dashboard/BudgetTotals.jsx
@@ -15,14 +15,13 @@ function BudgetTotals({ expenses }) {
   const averageSpend = prepareAverageSpend(expenses);
 
   return (
-    <div className="flex justify-around w-full mx-auto text-center p-4 rounded-xl md:w-fit md:space-x-4 md:h-fit">
+    <div className="flex justify-around w-full mx-auto text-center p-2 rounded-xl md:w-fit md:space-x-4 md:h-fit">
       <BudgetTotalsCard amount={formatCurrency(monthlyBudget)}>
         Monthly Budget
       </BudgetTotalsCard>
       <BudgetTotalsCard amount={formatCurrency(averageSpend)}>
         Average Spend
       </BudgetTotalsCard>
-      {/* <BudgetTotalsCard amount="$1,875.00">Net Cash Flow</BudgetTotalsCard> */}
     </div>
   );
 }
--- src/components/budget/dashboard/BudgetTotalsCard.jsx
+++ src/components/budget/dashboard/BudgetTotalsCard.jsx
@@ -1,6 +1,6 @@
 function BudgetTotalsCard({ children, amount }) {
   return (
-    <div className="bg-[#505050] p-2 rounded-xl text-slate-200">
+    <div className="bg-[#505050] p-2 rounded-xl text-slate-200 w-full">
       <p className="text-lg text-[#48ff00]">{children}</p>
       <p className="text-xl">{amount}</p>
     </div>
