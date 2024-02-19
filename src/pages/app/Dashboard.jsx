1094a0cd7b87b1aa9d89475c2c9702ea5c0b64a2
JustinRivera-9 <justin_rivera9@outlook.com>
2024-02-18 19:06:20 -0700

    Added message directing users to budget

--- src/pages/app/Dashboard.jsx
+++ src/pages/app/Dashboard.jsx
@@ -1,7 +1,16 @@
 function Dashboard() {
   return (
-    <div className="flex justify-center text-3xl mt-24">
-      <h1>INSERT DASHBOARD PAGE</h1>
+    <div className="flex flex-col text-center w-4/5 mx-auto mt-12 bg-[#494949] p-4 rounded-2xl space-y-12">
+      <p className="text-xl">
+        Please use the icons below to navigate to the app. This landing page is
+        reserved for the application wide dashboard.
+      </p>
+      <p className="text-xl">
+        <span className="text-[#48ff00]">Bug Discovered: </span>There is
+        currently a bug when trying to edit an expense that crashes the
+        application. If this happens to you please refresh and head back to the
+        planned page to reload the data.
+      </p>
     </div>
   );
 }
