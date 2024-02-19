9f988b534b07240e42f37d22fca4318619a965a6
JustinRivera-9 <justin_rivera9@outlook.com>
2024-02-18 19:14:51 -0700

    UI - Updated styles

--- src/components/budget/dashboard/UpcomingFixedExpenses.jsx
+++ src/components/budget/dashboard/UpcomingFixedExpenses.jsx
@@ -2,7 +2,7 @@ import { formatCurrency } from "../../../utils/helperFunctions";
 
 function UpcomingFixedExpenses({ expenses }) {
   const fixedExpenses = expenses.filter((el) => el.isFixed);
-
+  console.log(fixedExpenses);
   return (
     <div className="flex flex-col bg-[#505050] rounded-xl p-2 md:w-1/3 md:h-fit md:mt-4">
       <p className="text-slate-200 mb-4 text-center text-3xl">
@@ -12,13 +12,17 @@ function UpcomingFixedExpenses({ expenses }) {
         {fixedExpenses.map((item) => (
           <ul
             key={item.categoryName}
-            className="bg-[#292929] px-4 py-2 rounded-xl w-[48%] my-1"
+            className="flex items-center justify-between space-x-2 bg-[#292929] p-4 rounded-xl w-[48%] my-1"
           >
-            <li className="text-[#48ff00]">{item.date}</li>
-            <li className="text-lg text-slate-300">{item.categoryName}</li>
-            <li className="text-slate-200">
-              {formatCurrency(item.plannedAmount, true)}
+            <li className="text-xl text-[#48ff00] bg-[#494949] rounded-xl w-[30%] p-2">
+              {item.fixedDate}
             </li>
+            <div className="text-right">
+              <li className="text-sm text-slate-300">{item.categoryName}</li>
+              <li className="text-slate-200">
+                {formatCurrency(item.plannedAmount, true)}
+              </li>
+            </div>
           </ul>
         ))}
       </div>
