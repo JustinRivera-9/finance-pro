0fc103ff1a11dbdeea7e616f2da9c79d9a0b38a8
JustinRivera-9 <justin_rivera9@outlook.com>
2024-02-18 19:06:09 -0700

    Added demo credentials

--- src/ui/AuthForm.jsx
+++ src/ui/AuthForm.jsx
@@ -11,14 +11,23 @@ function AuthForm() {
   });
 
   return (
-    <div className="flex justif-center">
-      <div className="w-1/3 rounded-xl bg-zinc-700 px-8 py-4 mx-auto">
+    <div className="flex flex-col justify-center w-full mt-12 space-y-10">
+      <div className="rounded-xl bg-zinc-700 px-6 py-4 mx-auto w-4/5">
         <Auth
           supabaseClient={supabase}
           appearance={{ theme: ThemeSupa }}
           theme="dark"
-          providers={["google", "facebook", "apple"]}
+          providers={["google"]}
         />
+      </div>
+      <div className="flex flex-col mx-auto bg-[#494949] p-6 rounded-xl">
+        <p className="text-2xl text-[#48ff00] mb-4">Demo Account Credentials</p>
+        <p className="text-xl mb-2">
+          <span className="font-bold mr-4">Email: </span>john.doe@email.com
+        </p>
+        <p className="text-xl">
+          <span className="font-bold mr-4">Password: </span>Finance123
+        </p>
       </div>
     </div>
   );
