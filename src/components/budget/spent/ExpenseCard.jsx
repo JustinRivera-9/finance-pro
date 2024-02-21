611fa34e0c5871b7c7af0e33dd508161b9ef6834
JustinRivera-9 <justin_rivera9@outlook.com>
2024-02-20 20:26:35 -0700

    UI - Updated to prevent overflow

--- src/components/budget/spent/ExpenseCard.jsx
+++ src/components/budget/spent/ExpenseCard.jsx
@@ -6,15 +6,15 @@ function ExpenseCard({ expense }) {
   const [month, day] = date.split(" ");
 
   return (
-    <li className="flex mt-2 px-3 py-1 justify-between bg-[#292929] items-center w-full rounded-2xl">
-      <div className="flex space-x-4 items-center">
+    <li className="flex mt-2 px-2 py-1 justify-between bg-[#292929] items-center w-full rounded-2xl">
+      <div className="flex space-x-3 items-center pl-1">
         <div className="flex flex-col text-slate-300">
           <p className="text-2xl">{day}</p>
           <p>{month}</p>
         </div>
-        <p className="text-xl text-left">{description}</p>
+        <p className="text-base text-left">{description}</p>
       </div>
-      <div className="flex items-center space-x-4">
+      <div className="flex items-center space-x-1">
         <p className="text-[#48ff00bd] text-xl">{formatCurrency(amount)}</p>
         <ExpenseCardActions expense={expense} />
         {/* <MoreVertIcon /> */}
