5bf50cb5df1250d05e683c4b55637d3aead58844
JustinRivera-9 <justin_rivera9@outlook.com>
2024-02-20 20:36:57 -0700

    UI - Updated text size to prevent overflow

--- src/components/investing/portfolio/StockCard.jsx
+++ src/components/investing/portfolio/StockCard.jsx
@@ -10,16 +10,16 @@ function StockCard({ data }) {
 
   return (
     <div className="flex items-center space-x-6">
-      <div className="text-2xl">{logo}</div>
+      <div className="text-lg">{logo}</div>
       <div className="flex flex-col text-left">
-        <p className="text-2xl text-slate-200">
-          {name} <span className="text-xl text-slate-300">({ticker})</span>
+        <p className="text-xl text-slate-200">
+          {name} <span className="text-lg text-slate-300">({ticker})</span>
         </p>
         <div className="flex space-x-2 items-baseline">
-          <p className="text-2xl text-slate-200">
+          <p className="text-lg text-slate-200">
             {formatCurrency(totalPosition, true)}
           </p>
-          <p className={`text-${textColor} text-xl`}>
+          <p className={`text-${textColor} text-base`}>
             {`${positiveDay ? "+" : ""}${formatCurrency(
               investment.dayChange.dollarChange
             )}`}
